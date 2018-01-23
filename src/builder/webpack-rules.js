a6a3bd6787570a3bf9db878c920058cdc23df152
ecommercedept <ecommercedept@petsafe.net>
2018-01-23 13:03:27 -0500

    Performance improvements
    Performance improvements by removing the scope of what webpack does -- we don't need it for all the fanciness baked into Laravel Mix.

--- src/builder/webpack-rules.js
+++ src/builder/webpack-rules.js
@@ -17,20 +17,6 @@ module.exports = function () {
         ]
     });
 
-
-    // TypeScript Compilation.
-    if (Mix.isUsing('typeScript')) {
-        rules.push({
-            test: /\.tsx?$/,
-            loader: 'ts-loader',
-            exclude: /node_modules/,
-            options: {
-                appendTsSuffixTo: [/\.vue$/],
-            }
-        });
-    }
-
-
     // CSS Compilation.
     rules.push({
         test: /\.css$/,
@@ -39,96 +25,17 @@ module.exports = function () {
         loaders: ['style-loader', 'css-loader']
     });
 
-
     // Recognize .scss Imports.
     rules.push({
         test: /\.s[ac]ss$/,
         exclude: Config.preprocessors.sass ? Config.preprocessors.sass.map(sass => sass.src.path()) : [],
         loaders: ['style-loader', 'css-loader', 'sass-loader']
     });
-
-
-    // Recognize .less Imports.
-    rules.push({
-        test: /\.less$/,
-        exclude: Config.preprocessors.less ? Config.preprocessors.less.map(less => less.src.path()) : [],
-        loaders: ['style-loader', 'css-loader', 'less-loader']
-    });
-
-
-    // Add support for loading HTML files.
-    rules.push({
-        test: /\.html$/,
-        loaders: ['html-loader']
-    });
-
-
-    // Add support for loading images.
-    rules.push({
-        test: /\.(png|jpe?g|gif)$/,
-        loaders: [
-            {
-                loader: 'file-loader',
-                options: {
-                    name: path => {
-                        if (! /node_modules|bower_components/.test(path)) {
-                            return Config.fileLoaderDirs.images + '/[name].[ext]?[hash]';
-                        }
-
-                        return Config.fileLoaderDirs.images + '/vendor/' + path
-                            .replace(/\\/g, '/')
-                            .replace(
-                                /((.*(node_modules|bower_components))|images|image|img|assets)\//g, ''
-                            ) + '?[hash]';
-                    },
-                    publicPath: Config.resourceRoot
-                }
-            },
-
-            {
-                loader: 'img-loader',
-                options: Config.imgLoaderOptions
-            }
-        ]
-    });
-
-
-    // Add support for loading fonts.
-    rules.push({
-        test: /\.(woff2?|ttf|eot|otf)$/,
-        loader: 'file-loader',
-        options: {
-            name: path => {
-                if (! /node_modules|bower_components/.test(path)) {
-                    return Config.fileLoaderDirs.fonts + '/[name].[ext]?[hash]';
-                }
-
-                return Config.fileLoaderDirs.fonts + '/vendor/' + path
-                    .replace(/\\/g, '/')
-                    .replace(
-                        /((.*(node_modules|bower_components))|fonts|font|assets)\//g, ''
-                    ) + '?[hash]';
-            },
-            publicPath: Config.resourceRoot
-        }
-    });
-    
     
     // Add support for inline-SVGs
     rules.push({
         test: /\.svg$/,
         loader: 'raw-loader',
-    });
-
-
-    // Add support for loading cursor files.
-    rules.push({
-        test: /\.(cur|ani)$/,
-        loader: 'file-loader',
-        options: {
-            name: '[name].[ext]?[hash]',
-            publicPath: Config.resourceRoot
-        }
     });
 
 
@@ -201,69 +108,6 @@ module.exports = function () {
             });
         });
     });
-
-
-    // Vue Compilation.
-    let vueExtractPlugin;
-
-    if (Config.extractVueStyles) {
-        vueExtractPlugin = extractPlugins.length ? extractPlugins[0] : new ExtractTextPlugin('vue-styles.css');
-    }
-
-    rules.push({
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        exclude: /bower_components/,
-        options: {
-            // extractCSS: Config.extractVueStyles,
-            loaders: Config.extractVueStyles ? {
-                js: {
-                    loader: 'babel-loader',
-                    options: Config.babel()
-                },
-
-                scss: vueExtractPlugin.extract({
-                    use: 'css-loader!sass-loader',
-                    fallback: 'vue-style-loader'
-                }),
-
-                sass: vueExtractPlugin.extract({
-                    use: 'css-loader!sass-loader?indentedSyntax',
-                    fallback: 'vue-style-loader'
-                }),
-
-                css: vueExtractPlugin.extract({
-                    use: 'css-loader',
-                    fallback: 'vue-style-loader'
-                }),
-
-                stylus: vueExtractPlugin.extract({
-                    use: 'css-loader!stylus-loader?paths[]=node_modules',
-                    fallback: 'vue-style-loader'
-                }),
-
-                less: vueExtractPlugin.extract({
-                    use: 'css-loader!less-loader',
-                    fallback: 'vue-style-loader'
-                }),
-            } : {
-                js: {
-                    loader: 'babel-loader',
-                    options: Config.babel()
-                }
-            },
-            postcss: Config.postCss,
-            preLoaders: Config.vue.preLoaders,
-            postLoaders: Config.vue.postLoaders,
-            esModule: Config.vue.esModule
-        }
-    });
-
-    // If there were no existing extract text plugins to add our
-    // Vue styles extraction too, we'll push a new one in.
-    if (Config.extractVueStyles && ! extractPlugins.length) {
-        extractPlugins.push(vueExtractPlugin);
-    }
 
     return { rules, extractPlugins };
 }
